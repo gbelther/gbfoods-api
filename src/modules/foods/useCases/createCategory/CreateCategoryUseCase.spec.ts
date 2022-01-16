442c0a9e341579defed0ac1493d65821bfebe90a
GBelther <belthergabriel@gmail.com>
2022-01-16 13:49:53 -0300

    test: criação dos testes de de regras de negócio da criação de categoria

--- src/modules/foods/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/foods/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from "../../../../shared/errors/AppError";
 import { CategoriesRepositoryInMemory } from "../../infra/typeorm/repositories/in-memory/CategoriesRepositoryInMemory";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
@@ -23,8 +24,43 @@ describe("Create Category", () => {
       description: category.description,
     });
 
-    console.log(categoryCreated);
-
     expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with invalids params", async () => {
+    const category = {
+      name: null,
+      description: "Categoria teste 1 - descrição",
+    };
+
+    await expect(
+      createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+    ).rejects.toEqual(
+      new AppError("Os parâmetros name e description são obrigatórios!")
+    );
+  });
+
+  it("should not be able to create another category with a existing name", async () => {
+    const category = {
+      name: "Categoria teste 1",
+      description: "Categoria teste 1 - descrição",
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    await expect(
+      createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      })
+    ).rejects.toEqual(
+      new AppError(`A categoria com o nome ${category.name} já existe!`)
+    );
   });
 });
