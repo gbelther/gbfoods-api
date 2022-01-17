a13c0641878453e35c480064670196fa6161cd55
GBelther <belthergabriel@gmail.com>
2022-01-17 20:08:37 -0300

    feature: criação de usuário

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
+import { UsersRepository } from "../../modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { CategoriesRepository } from "../../modules/foods/infra/typeorm/repositories/CategoriesRepository";
 import { ICategoriesRepository } from "../../modules/foods/repositories/ICategoriesRepository";
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 );
