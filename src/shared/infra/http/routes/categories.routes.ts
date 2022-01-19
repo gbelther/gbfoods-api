c27a19d3c33cf938ec18e8b9dae994a568317099
GBelther <belthergabriel@gmail.com>
2022-01-18 21:02:34 -0300

    feature: criação da autenticação das rotas

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateCategoryController } from "../../../../modules/foods/useCases/createCategory/CreateCategoryController";
 import { DeleteCategoryController } from "../../../../modules/foods/useCases/deleteCategory/DeleteCategoryController";
 import { ListCategoriesController } from "../../../../modules/foods/useCases/listCategories/ListCategoriesController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
 
@@ -10,8 +11,16 @@ const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 const deleteCategoryController = new DeleteCategoryController();
 
-categoriesRoutes.post("/", createCategoryController.handle);
-categoriesRoutes.get("/", listCategoriesController.handle);
-categoriesRoutes.delete("/:id", deleteCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  createCategoryController.handle
+);
+categoriesRoutes.get("/", ensureAuthenticated, listCategoriesController.handle);
+categoriesRoutes.delete(
+  "/:id",
+  ensureAuthenticated,
+  deleteCategoryController.handle
+);
 
 export { categoriesRoutes };
